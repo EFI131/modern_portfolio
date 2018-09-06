541af29c7e97cd8f46a4c6a2d3377d735bd2968b
Efi Shtainer <efi131@gmail.com>
2018-09-06 20:46:24 +0800

    menu overlay

--- dist/js/main.js
+++ dist/js/main.js
@@ -3,7 +3,7 @@ const menuBtn = document.querySelector('.menu-btn');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBranding = document.querySelector('.menu-branding');
-const navItems = document.querySelectorAll('nav-item');
+const navItems = document.querySelectorAll('.nav-item');
 
 //Set initial state of menu
 let showMenu = false;
@@ -16,7 +16,7 @@ function toggleMenu() {
         menu.classList.add('show');
         menuNav.classList.add('show');
         menuBranding.classList.add('show');
-        navItems.forEach(item => ite.classList.add('show'));
+        navItems.forEach(item => item.classList.add('show'));
 
         // set manu status
         showMenu = true;
@@ -26,7 +26,7 @@ function toggleMenu() {
         menu.classList.remove('show');
         menuNav.classList.remove('show');
         menuBranding.classList.remove('show');
-        navItems.forEach(item => ite.classList.remove('show'));
+        navItems.forEach(item => item.classList.remove('show'));
 
         // set menu status
         showMenu = false;
